c26b965157d955fd4df7e1828628df6e1a86df6a
MaksimNorvatkin <norvatkin_maksim@mail.ru>
2026-04-12 00:54:33 +0300

    добавил StadiumRepository и TeamRepository

---
+++ src/main/java/ru/footballticket/repository/StadiumRepository.java
@@ -0,0 +1,9 @@
+package ru.footballticket.repository;
+
+import ru.footballticket.entity.Stadium;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+}
---
+++ src/main/java/ru/footballticket/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package ru.footballticket.repository;
+
+import ru.footballticket.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
